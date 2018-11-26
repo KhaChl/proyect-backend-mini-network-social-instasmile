85aee7b915eb4b1baf30b90d3b6da918690c6109
Kevin <kpizarro.m@outlook.com>
2018-11-25 21:16:46 -0300

    Add helper format time

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        App\Providers\FormatTimeServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -210,6 +211,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'FormatTime' => App\Helpers\FormatTime::class,
 
     ],
 
